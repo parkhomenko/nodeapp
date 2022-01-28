c9eb7df14cc8a0e62f66b8643ef09f27cf2928d9
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2022-01-28 15:55:21 +0200

    test deleting eb config file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use('/', (req, res) => {
-  res.send(`<h1>Hello Elastic Beanstalk</h1>`);
+  res.send(`<h1>Hello Elastic Beanstalk, here we are!</h1>`);
 });
 
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
