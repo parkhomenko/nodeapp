4ab02bb962f7bf7e83191f75f443e99b73750e31
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2022-01-28 13:03:26 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/', (req, res) => {
+  res.send(`<h1>Hello World</h1>`);
+});
+
+app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
